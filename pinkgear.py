6a2b43ce87dfbebfbd5621da2550a96920e58276
barmalie <6r0d5k1y@gmail.com>
2026-02-15 11:18:17 +0300

    создание репозитория приложения pinkgear

---
+++ pinkgear.py
@@ -0,0 +1,221 @@
+import math
+
+z1 = round((int(input("z шестерни:",))),3)
+z2 = round((int(input("z колеса:",))),3)
+mte = round((float(input("mte:",))),3)
+mn = round((float(input("mn:",))),3)
+b = round((float(input("b:",))),3)
+alpha = 20 #tg20
+alpharadians = math.radians(alpha)
+tgalpharadians = round(math.tan(alpharadians),4)
+beta = 35 #tg35
+betaradians = math.radians(beta)
+tgbetaradians = round(math.tan(betaradians),4)
+d0 = round((float(input("диаметр головки(do):",))),3)
+ru = d0/2
+zc = round((math.sqrt(z1**2 + z2**2)),6)
+print("zc:",zc)
+Re = mte * (zc / 2)
+R = Re - (b / 2)
+
+Bm = int(input("средний угол наклона спирали(Bn):",))
+cosBm = round(math.cos(math.radians(Bm)),5)
+sinBm = round(math.sin(math.radians(Bm)),4)
+
+sumQf = math.floor((((10800*tgbetaradians)/tgalpharadians)-((((10800*tgbetaradians)/tgalpharadians) * ((sinBm/ru)*R))))/(zc *sinBm))
+
+V0 = round((ru * cosBm), 5)
+H0 = R - (ru * sinBm)
+U0 = round(math.sqrt(V0**2 + H0**2),6)
+
+q_rad = math.atan2(V0, H0)
+q = math.degrees(q_rad)
+q_1 = math.floor(q)
+q_degrees = round((((q - q_1)/100*60) + q_1), 2)
+print("q",q_degrees)
+sin_q = round(math.sin(math.radians(q)), 6)
+empty11 = round((U0/270), 6)
+sin_e02 = empty11
+degrees_sin_e0 = math.asin(sin_e02)
+degrees_sin_e0_1 = round(math.degrees(degrees_sin_e0),6)
+degrees_sin_e0_2 = degrees_sin_e0_1 * 2
+degrees_sin_e0_3 = math.floor(degrees_sin_e0_2)
+degrees_sin_e0_4 = round((((degrees_sin_e0_2 - degrees_sin_e0_3)/100*60) + degrees_sin_e0_3),2)
+print("e0:", degrees_sin_e0_4)
+degrees_sin_vip  = degrees_sin_e0_4 - 0.25
+degrees_sin_vog  = degrees_sin_e0_4 - 0.1
+print("e0_выпуклая:",degrees_sin_vip,"e0_вогнутая:",degrees_sin_vog)
+
+Q_left= degrees_sin_e0_1 + q
+Q_left_1 = math.floor(Q_left)
+Q_left_degrees = round((((Q_left - Q_left_1)/100*60) + Q_left_1), 2)
+print("Q_левое",Q_left_degrees)
+
+Q_right = degrees_sin_e0_1 - q
+Q_right_1 = math.floor(Q_right)
+Q_right_degrees_1 = round((((Q_right - Q_right_1)/100*60) + Q_right_1), 2)
+if Q_right_degrees_1 <= 0:
+    Q_right_degrees_1 = Q_right + 360
+    Q_right_degrees_2 = math.floor(Q_right_degrees_1)
+    Q_right_degrees_3 = round((((Q_right_degrees_1 - Q_right_degrees_2)/ 100*60) + Q_right_degrees_2),2)#88_2
+print("Q_правое:", Q_right_degrees_3)
+
+deltacons = math.floor(sumQf)
+
+N0 = round(((deltacons/20) * sinBm),3)
+print("номер резца ",N0)
+
+Nf = math.ceil((deltacons/20) * sinBm)
+        
+print("номер резца рекомендуемый:",Nf)
+               
+sinBm1 = round(((20*Nf)/deltacons),6)
+Bm1 = math.asin(sinBm1)
+asinBm1 = round(math.degrees(Bm1),6)
+greesBm1 = math.floor(asinBm1)
+#print("Угол наклона фактический:", greesBm1)
+minutesBm1 = round(((((asinBm1) - greesBm1)/100*60) + greesBm1), 2)
+cosBm1  = round((math.cos(math.radians(asinBm1))),6)
+
+Qf = sumQf / 2
+Qf1_1 = Qf / 60
+tgQf2 = math.tan(math.radians(Qf1_1))
+cosQf2 = round((math.cos(math.radians(Qf1_1))),4)
+
+
+hm = 2.25 * mn
+h = round((hm + (b * tgQf2)), 3)
+print("h высота у большого модуля:",h)
+               
+zi = int(input("введите число не кратное ни колесу ни шестерне от 7 до 17:",))
+idsh = round((2 * (zi / z1)),6)
+idk = round((2 * (zi / z2)),6)
+print("гитара деления для шестерни", idsh)
+for gear_1 in range(34, 91):
+    for gear_2 in range(34, 91):
+        variant_2gears = gear_1 / gear_2
+        variant_2gears = round((variant_2gears), 7)
+        diff_1 = idsh - 0.000001
+        diff_2 = idsh + 0.000001
+        if diff_1 < variant_2gears < diff_2:
+            print(gear_1, "/", gear_2, "=", variant_2gears)
+            break
+        else:
+            variant_4gears_1 = gear_1 * gear_2
+            for gear_3 in range(34,91):
+                for gear_4 in range(34,91):
+                    variant_4gears_2 = gear_3 * gear_4
+                    variant_4gears = round((variant_4gears_1 / variant_4gears_2), 7)
+                    if diff_1 <= variant_4gears <= diff_2:
+                        break
+                        print()
+                        print(gear_1, "*", gear_2,)
+                        print("--    --", "=", variant_4gears)
+                        print(gear_3, "*", gear_4,)
+                        print()
+                        
+         
+print("гитара деления для колеса",idk)
+for gear_1 in range(34, 91):
+    for gear_2 in range(34, 91):
+        variant_2gears = gear_1 / gear_2
+        variant_2gears = round((variant_2gears), 7)
+        diff_1 = idk - 0.000001
+        diff_2 = idk + 0.000001
+        if diff_1 < variant_2gears < diff_2:
+            print(gear_1, "/", gear_2, "=", variant_2gears)
+            break
+        else:
+            variant_4gears_1 = gear_1 * gear_2
+            for gear_3 in range(34,91):
+                for gear_4 in range(34,91):
+                    variant_4gears_2 = gear_3 * gear_4
+                    variant_4gears = round((variant_4gears_1 / variant_4gears_2), 7)
+                    if diff_1 <= variant_4gears <= diff_2:
+                        break
+                        print()
+                        print(gear_1, "*", gear_2,)
+                        print("--    --", "=", variant_4gears)
+                        print(gear_3, "*", gear_4,)
+                        print()
+
+i_exemple_0 = round((3.5 * (zi/zc)),6)
+print("обкат для колеса:", i_exemple_0)
+
+
+
+for gear_1 in range(34, 91):
+    for gear_2 in range(34, 91):
+        variant_2gears = gear_1 / gear_2
+        variant_2gears = round((variant_2gears), 7)
+        diff_1 = i_exemple_0 - 0.000001
+        diff_2 = i_exemple_0 + 0.000001
+        if diff_1 < variant_2gears < diff_2:
+            print(gear_1, "/", gear_2, "=", variant_2gears)
+            break
+        else:
+            variant_4gears_1 = gear_1 * gear_2
+            for gear_3 in range(34,91):
+                for gear_4 in range(34,91):
+                    variant_4gears_2 = gear_3 * gear_4
+                    variant_4gears = round((variant_4gears_1 / variant_4gears_2), 7)
+                    if diff_1 <= variant_4gears <= diff_2:
+                        print()
+                        print(gear_1, "*", gear_2,)
+                        print("--   --", "=", variant_4gears)
+                        print(gear_3, "*", gear_4,)
+                        print()
+
+
+i_exemple_1 = round(((3.5 * zi)/(zc - 0.25)),6)
+print("обкат для выпуклой:", i_exemple_1)
+
+for gear_1 in range(34, 91):
+    for gear_2 in range(34, 91):
+        variant_2gears = gear_1 / gear_2
+        variant_2gears = round((variant_2gears), 7)
+        diff_1 = i_exemple_1 - 0.000001
+        diff_2 = i_exemple_1 + 0.000001
+        if diff_1 < variant_2gears < diff_2:
+            print(gear_1, "/", gear_2, "=", variant_2gears)
+            break
+        else:
+            variant_4gears_1 = gear_1 * gear_2
+            for gear_3 in range(34,91):
+                for gear_4 in range(34,91):
+                    variant_4gears_2 = gear_3 * gear_4
+                    variant_4gears = round((variant_4gears_1 / variant_4gears_2), 7)
+                    if diff_1 <= variant_4gears <= diff_2:
+                        print()
+                        print(gear_1, "*", gear_2,)
+                        print("--   --", "=", variant_4gears)
+                        print(gear_3, "*", gear_4,)
+                        print()
+
+i_exemple_2 = round(((3.5 * zi)/(zc + 0.2)),6)
+print("обкат для вогнутой:", i_exemple_2)
+
+for gear_1 in range(34, 91):
+    for gear_2 in range(34, 91):
+        variant_2gears = gear_1 / gear_2
+        variant_2gears = round((variant_2gears), 7)
+        diff_1 = i_exemple_2 - 0.000001
+        diff_2 = i_exemple_2 + 0.000001
+        if diff_1 < variant_2gears < diff_2:
+            print(gear_1, "/", gear_2, "=", variant_2gears)
+            break
+        else:
+            variant_4gears_1 = gear_1 * gear_2
+            for gear_3 in range(34,91):
+                for gear_4 in range(34,91):
+                    variant_4gears_2 = gear_3 * gear_4
+                    variant_4gears = round((variant_4gears_1 / variant_4gears_2), 7)
+                    if diff_1 <= variant_4gears <= diff_2:
+                        print()
+                        print(gear_1, "*", gear_2,)
+                        print("--   --", "=", variant_4gears)
+                        print(gear_3, "*", gear_4,)
+                        print()                        
+
+    
+input("Нажмите для выхода...")
